c2de68d40981952469c06004a68ed95c6847869c
Tibo Stroo <tibo.stroo@hotmail.be>
2021-02-04 15:08:47 +0100

    Initial commit

---
+++ hangman.py
@@ -0,0 +1,45 @@
+import random
+import functions as f
+playgame = True
+while playgame:
+
+    WORD = random.choice(open("hangmanwords.txt").read().split("\n")) # the word that has to be guessed
+    tries = 0
+    temp_word = "*" * len(WORD)
+    guess = ""
+    guessed_chars = []
+
+
+    name = str(input("Enter your name\n> "))
+    print("Hello " + name + ", you're playing the game 'hangman'. You have 10 tries to guess a randomly chosen word of " 
+                + str(len(WORD)) + " characters. GLHF!")
+
+
+    print("woord is " + WORD + " met lengte " + str(len(WORD)))
+    print(str(len(WORD)))
+
+    while "*" in temp_word and guess != WORD:
+        guess=""
+        while len(guess) != len(WORD) and len(guess) != 1 or guess in guessed_chars:
+            guess = (str(input("Guess a character or try to guess the " + str(len(WORD)) + " letter word. (These are the characters that you have already tried : " + str(guessed_chars) + ")\n> ")))
+            print(guess)    
+        
+
+        if (guess == WORD):
+            print("Congratiolations, " + name + " you guessed the correct word in " + str(tries) + "tries!!")
+          
+
+        elif guess in WORD:
+            print("Your guess was correct!")
+            temp_word = f.replace_char_in_word(guess, WORD, temp_word)
+            print(temp_word)
+        
+        else:
+            guessed_chars.append(guess)
+            tries+=1
+            f.draw_image(tries)
+
+        
+    answer = str(input("Play again? (y/n)\n> "))
+    playgame = answer.lower() == "y"
+
---
+++ test.py
@@ -0,0 +1,7 @@
+from functions import replace_char_in_word
+
+print(replace_char_in_word("p", "appel", "a____"))
+print(replace_char_in_word("l", "appel", "a____"))
+
+
+
